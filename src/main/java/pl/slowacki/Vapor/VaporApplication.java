310b646c9540e35c36f5952f15654680546e475c
Jakub Pietrala <pietrala.jakub05@gmail.com>
2022-11-07 17:46:56 +0100

    Initial commit

---
+++ src/main/java/pl/slowacki/Vapor/VaporApplication.java
@@ -0,0 +1,13 @@
+package pl.slowacki.Vapor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaporApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaporApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/slowacki/Vapor/VaporApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.slowacki.Vapor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaporApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
